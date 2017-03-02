01fac12bca37e4c418abaeed8b95118ade8e811e
Matei Copot <matei@copot.eu>
2017-03-02 00:18:16 +0000

    added ` : x(x), y(y) {` notation in Maze.cpp

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -1,13 +1,7 @@
 #include "Coordinate.h"
 
-Coordinate::Coordinate(){
-  this->x = 0;
-  this->y = 0;
-}
-Coordinate::Coordinate( int x, int y ){
-  this->x = x;
-  this->y = y;
-}
+Coordinate::Coordinate() : x(0), y(0) {}
+Coordinate::Coordinate( int x, int y ) : x(x), y(y) {}
 Coordinate operator + ( Coordinate a, const Coordinate& b ){
   return Coordinate( a.x + b.x, a.y + b.y );
 }
